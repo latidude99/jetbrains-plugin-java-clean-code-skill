f9bf35f45de2855e2649ccba58e53b6822242943
GitHub Action <action@github.com>
2026-03-25 18:27:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/latidude99/jetbrainspluginjavacleancodeskill/MyBundle.kt
+++ src/main/kotlin/com/github/latidude99/jetbrainspluginjavacleancodeskill/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.latidude99.jetbrainspluginjavacleancodeskill
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/latidude99/jetbrainspluginjavacleancodeskill/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/latidude99/jetbrainspluginjavacleancodeskill/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.latidude99.jetbrainspluginjavacleancodeskill.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.latidude99.jetbrainspluginjavacleancodeskill.MyBundle
+import com.github.latidude99.jetbrainspluginjavacleancodeskill.services.MyProjectService
 import javax.swing.JButton
 
 
